975e21c0ee79aee91bcfb796f04bd38682eba9ba
ASFA-TARIQ <asfatariq123@gmail.com>
2026-04-04 00:24:28 +0400

    Home Page

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("active");
+  navMenu.classList.toggle("show");
+});
